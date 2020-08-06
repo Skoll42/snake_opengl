4fdc8064d4b585b3c5e96f1783d3cf40bf40ad55
Andrew Kostko <andrew@sobi.design>
2020-08-06 22:32:47 +0300

    Initial commit

---
+++ SnakeOpenGL/SnakeOpenGL/SnakeOpenGL.cpp
@@ -0,0 +1,12 @@
+﻿// SnakeOpenGL.cpp : Defines the entry point for the application.
+//
+
+#include "SnakeOpenGL.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello Snake." << endl;
+	return 0;
+}
